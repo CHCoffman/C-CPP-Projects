aac41a68d04c13cdc57c128092504dbadfc851af
Colten <47945774+CHCoffman@users.noreply.github.com>
2019-10-08 15:05:54 -0500

    Add files via upload

---
+++ C++Chapter3.cpp
@@ -0,0 +1,17 @@
+// Colten Coffman C++ Practice
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int intt;
+	double dub;
+	cout << "Enter a double and an integer";
+	cin >> dub >> intt;
+	cout << "double entered :" << dub << endl;
+	cout << "integer entered: " << intt << endl;
+
+}
+
